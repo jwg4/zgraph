1ef60412cb19284a9dece8e3893e288a38933d90
Jack <jack.grahl@yahoo.co.uk>
2015-07-04 14:50:53 +0100

    Add triangulate function with basic test.

--- test/test_z_graph.py
+++ test/test_z_graph.py
@@ -5,3 +5,50 @@ Created on Sat Jul  4 14:08:38 2015
 @author: jack
 """
 
+import unittest
+
+import stl
+
+from z_graph import ZGraph
+
+class TestZGraph(unittest.TestCase):
+    def test_solid_output(self):
+        def const_2(x, y):
+            return 2
+        graph = ZGraph((0,1), (0,1), const_2, 1)
+        top_left_front     = stl.Vector3d(0, 0, 2)
+        top_right_front    = stl.Vector3d(1, 0, 2)
+        top_left_back      = stl.Vector3d(0, 1, 2)
+        top_right_back     = stl.Vector3d(1, 1, 2)
+        bottom_left_front  = stl.Vector3d(0, 0, 0)
+        bottom_right_front = stl.Vector3d(1, 0, 0)
+        bottom_left_back   = stl.Vector3d(0, 1, 0)
+        bottom_right_back  = stl.Vector3d(1, 1, 0)
+        facets = [
+             # Top
+            stl.Facet(stl.Vector3d(0, 0, 1),
+                      (top_left_front, top_right_front, top_right_back)),    
+            stl.Facet(stl.Vector3d(0, 0, 1),
+                      (top_right_back, top_left_back, top_left_front)),    
+            # Bottom
+            stl.Facet(stl.Vector3d(0, 0, -1),
+                      (bottom_left_front, bottom_right_back, bottom_right_front)),    
+            stl.Facet(stl.Vector3d(0, 0, -1),
+                      (bottom_right_back, bottom_left_back, bottom_left_front)),    
+        ]
+        expected = stl.Solid("ZGraph", facets)
+    
+    def test_triangulate(self):
+        top_left_front     = stl.Vector3d(0, 0, 2)
+        top_right_front    = stl.Vector3d(1, 0, 2)
+        top_left_back      = stl.Vector3d(0, 1, 2)
+        top_right_back     = stl.Vector3d(1, 1, 2)
+        facets = [top_left_front,
+                  top_left_back,
+                  top_right_back,
+                  top_right_front]  
+        normal = stl.Vector3d(0, 0, 1)
+        result = ZGraph.triangulate(normal, facets)
+        self.assertEqual(len(result), 2)
+        
+    
--- z_graph.py
+++ z_graph.py
@@ -5,3 +5,23 @@ Created on Sat Jul  4 14:03:34 2015
 @author: jack
 """
 
+import stl
+
+class ZGraph(object):
+    def __init__(self, xrange, yrange, f, n):
+        self.xrange = xrange
+        self.yrange = yrange
+        self.f = f
+        self.n = n
+        
+    @staticmethod
+    def triangulate(normal, vertices):
+        if len(vertices) < 3:
+            return []
+        facet1 = stl.Facet(normal, vertices[:3])
+        remaining_facets = vertices[:1] + vertices[2:]
+        return [facet1] + ZGraph.triangulate(normal, remaining_facets)
+        
+        
+        
+        
