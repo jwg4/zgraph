8e1f851512c4e5ef6cb5aa712ec63dcc1cf9a8f2
Jack <jack.grahl@yahoo.co.uk>
2015-07-04 16:23:28 +0100

    Add a second test for solid_output().

--- test/test_z_graph.py
+++ test/test_z_graph.py
@@ -12,7 +12,7 @@ import stl
 from z_graph import ZGraph
 
 class TestZGraph(unittest.TestCase):
-    def test_solid_output(self):
+    def test_solid_output_for_const_graph(self):
         def const_2(x, y):
             return 2
         graph = ZGraph((0,1), (0,1), const_2, 1)
@@ -34,6 +34,90 @@ class TestZGraph(unittest.TestCase):
                 top_left_back
             )
         )
+        bottom = ZGraph.triangulate(
+            stl.Vector3d(0, 0, -1),
+            (
+                bottom_left_front,
+                bottom_left_back,
+                bottom_right_back,
+                bottom_right_front
+            )
+        )
+        front = ZGraph.triangulate(
+            stl.Vector3d(0, -1, 0),
+            (
+                bottom_left_front,
+                bottom_right_front,
+                top_right_front,
+                top_left_front
+            )        
+        )
+        right = ZGraph.triangulate(
+            stl.Vector3d(1, 0, 0),
+            (
+                bottom_right_front,
+                bottom_right_back,
+                top_right_back,
+                top_right_front
+            )
+        )
+        back = ZGraph.triangulate(
+            stl.Vector3d(0, 1, 0),
+            (
+                bottom_left_back,
+                top_left_back,
+                top_right_back,
+                bottom_right_back
+            )        
+        )
+        left = ZGraph.triangulate(
+            stl.Vector3d(-1, 0, 0),
+            (
+                bottom_left_front,
+                top_left_front,
+                top_left_back,
+                bottom_left_back
+            )
+        )
+        facets = top + bottom + front + right + back + left
+        expected = stl.Solid("ZGraph", facets)
+        
+        self.assertEqual(graph.solid_output(), expected)
+    
+    def test_solid_output_for_simple_graph(self):
+        def simple(x, y):
+            if x > 0.5 and y > 0.5:            
+                return 2
+            else:
+                return 1
+        graph = ZGraph((0,1), (0,1), simple, 1)
+
+        top_left_front     = stl.Vector3d(0, 0, 1)
+        top_right_front    = stl.Vector3d(1, 0, 1)
+        top_left_back      = stl.Vector3d(0, 1, 1)
+        top_right_back     = stl.Vector3d(1, 1, 2)
+        bottom_left_front  = stl.Vector3d(0, 0, 0)
+        bottom_right_front = stl.Vector3d(1, 0, 0)
+        bottom_left_back   = stl.Vector3d(0, 1, 0)
+        bottom_right_back  = stl.Vector3d(1, 1, 0)
+        top = [ 
+                stl.Facet(
+                    stl.Vector3d(0, -1, 1),
+                    [
+                        top_left_front, 
+                        top_right_front,
+                        top_right_back
+                    ]                
+                ),
+                stl.Facet(
+                    stl.Vector3d(-1, 0, 1),
+                    [
+                        top_left_front, 
+                        top_right_back,
+                        top_left_back
+                    ]                
+                ),
+              ]        
         bottom = ZGraph.triangulate(
             stl.Vector3d(0, 0, -1),
             (
