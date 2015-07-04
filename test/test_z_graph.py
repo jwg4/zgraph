7169b16c91313a058602ca6561a760ddc8739dc6
Jack <jack.grahl@yahoo.co.uk>
2015-07-04 14:42:58 +0100

    Initial version of ZGraph with tests.

---
+++ test/test_z_graph.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 14:08:38 2015
+
+@author: jack
+"""
+
---
+++ z_graph.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 14:03:34 2015
+
+@author: jack
+"""
+
