9aacfa175e3e7f14e53082a4445247bb02be02fb
Jack <jack.grahl@yahoo.co.uk>
2015-07-10 00:06:21 +0100

    Make this graph slightly more convenient.

--- sample.py
+++ sample.py
@@ -3,10 +3,10 @@ from math import sin
 from z_graph import ZGraph
 
 PI = 3.14159
-RANGE = (0, PI/2)
+RANGE = (0, PI)
 
 def f(x, y):
-    return 4 * sin(x) * sin(y) + 2 * sin(x) + 2 * sin(y)  
+    return (4 * sin(x) * sin(y) + 2 * sin(x) + 2 * sin(y)) / 10
 
 if __name__ == "__main__":
     z = ZGraph(RANGE, RANGE, f, 100)
