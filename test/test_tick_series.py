8954ebab57c972ed0073aebb84fdf996bf844f1a
Jack <jack.grahl@yahoo.co.uk>
2015-07-09 23:58:56 +0100

    Test that the ticks are the correct values.

--- test/test_tick_series.py
+++ test/test_tick_series.py
@@ -13,6 +13,13 @@ class TestTickSeries(unittest.TestCase):
         series = TickSeries((0,1), 10)
         self.assertEqual(11, len(set(series.ticks)))
 
+    def test_tick_values(self):
+        series = TickSeries((0,1), 10)
+        expected = [x / 10.0 for x in range(0, 11)]
+        matched = zip(expected, series.ticks)
+        for a, b in matched:
+            self.assertAlmostEqual(a, b)
+
     def test_number_of_pairs(self):
         series = TickSeries((0,1), 10)
         self.assertEqual(10, len(list(series.pairs)))
