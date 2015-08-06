4fab933ea927120b2688ad790353660e46211bcb
Jack <jack.grahl@yahoo.co.uk>
2015-08-06 23:13:11 +0100

    Add test for side_face.
    Test fails - only 5 facets for a front face.

---
+++ test/test_side_face.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 22:57:55 2015
+
+@author: jack
+"""
+
+import unittest
+
+import stl
+
+from side_face import XFace
+from tick_series import TickSeries
+
+class MockZGraph(object):
+    def __init__(self, f, x_range, x_series):
+        self.vector_f = f
+        self.x_range = x_range
+        self.x_series = x_series
+
+class TestSideFace(unittest.TestCase):
+    def test_basic_steplike_face(self):
+        def f(x,y):
+            return stl.Vector3d(x, y, 2 if abs(x) > 1 else 1)
+        range_ = (-3, 3)
+        x_series = TickSeries(range_, 4)
+        graph = MockZGraph(f, range_, x_series)
+        
+        front_face = XFace(range_[0], stl.Vector3d(0, -1, 0), True, graph)
+        
+        self.assertEqual(len(front_face.face), 6)
